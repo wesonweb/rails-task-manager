c460c3e74a2320881872865cbf2d3b4ee618c18c
Wes Mehaffy <wesonweb@gmail.com>
2023-01-21 14:57:00 +0000

    add show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # Read all
+  get '/tasks', to: 'tasks#index'
+
+  # Create
+  # get form
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+
+  # post form
+  post '/tasks', to: 'tasks#create'
+  # Read one
+  get '/tasks/:id', to: 'tasks#show', as: :task
 end
