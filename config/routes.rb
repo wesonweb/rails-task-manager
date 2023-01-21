a161caca5daf811f6cb53b8f283b507d60a7e734
Wes Mehaffy <wesonweb@gmail.com>
2023-01-21 18:53:03 +0000

    add edit functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,12 @@ Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index'
 
   # Create
-  # get form
   get '/tasks/new', to: 'tasks#new', as: :new_task
-  # post form
   post '/tasks', to: 'tasks#create'
+
+  # Update
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
 
   # Read one
   get '/tasks/:id', to: 'tasks#show', as: :task
